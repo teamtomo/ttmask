d67cb06805dcce0b44fd6f217e1abddd67cc8e27
milesagraham <miles.graham98@gmail.com>
2024-05-17 11:13:35 +0100

    add initial sphere script as a cli option (#8)

--- src/ttmask/sphere.py
+++ src/ttmask/sphere.py
@@ -3,11 +3,13 @@ import numpy as np
 import einops
 import napari
 import typer
+from scipy.ndimage import distance_transform_edt
 
 @cli.command(name='sphere')
 def sphere(
     boxsize: int = typer.Option(...),
     sphere_diameter: float = typer.Option(...),
+    soft_edge_size: int = typer.Option(...),
 ):
 
     sphere_radius = sphere_diameter / 2
@@ -28,6 +30,12 @@ def sphere(
     print('calculating which pixels are in sphere')
     idx = distance < sphere_radius
     mask[idx] = 1
+
+    distance_from_edge = distance_transform_edt(mask == 0)
+    boundary_pixels = (distance_from_edge <= soft_edge_size) & (distance_from_edge != 0)
+    normalised_distance_from_edge = (distance_from_edge[boundary_pixels] / soft_edge_size) * np.pi
+
+    mask[boundary_pixels] = (0.5 * np.cos(normalised_distance_from_edge) + 0.5)
 
     viewer = napari.Viewer(ndisplay=3)
     viewer.add_image(mask)
