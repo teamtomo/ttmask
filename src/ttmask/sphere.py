55c5b02df420cbeab1a59affea1ab215f1640ca5
Miles Graham <miles.graham98@gmail.com>
2024-08-08 12:09:58 +0100

    sphere mistake fixed

--- src/ttmask/sphere.py
+++ src/ttmask/sphere.py
@@ -5,12 +5,12 @@ import numpy as np
 import typer
 import mrcfile
 
-from ._cli import cli
-from .soft_edge import add_soft_edge
-from .box_setup import box_setup
+#from ._cli import cli
+from soft_edge import add_soft_edge
+from box_setup import box_setup
 
 
-@cli.command(name='sphere')
+#@cli.command(name='sphere')
 def sphere(
     sidelength: int = typer.Option(...),
     sphere_diameter: float = typer.Option(...),
@@ -25,7 +25,7 @@ def sphere(
     coordinates_centered, mask = box_setup(sidelength)
 
     #determine distances of each pixel to the center
-    distance_to_center = np.linalg.norm(coordinates_centered)
+    distance_to_center = np.linalg.norm(coordinates_centered, axis=-1)
 
     # set up criteria for which pixels are inside the sphere and modify values to 1.
     inside_sphere = distance_to_center < (sphere_radius / pixel_size)
@@ -41,3 +41,5 @@ def sphere(
 
     # output created with desired pixel size.
     mrcfile.write(output, mask, voxel_size=pixel_size, overwrite=True)
+
+sphere(100,30,0,1,"sphere.mrc",0)
