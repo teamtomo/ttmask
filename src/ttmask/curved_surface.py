c4eda24841928e0e6da7db418f1c7bb60978a369
Johannes Elferich <jojotux123@hotmail.com>
2024-09-09 21:18:01 -0400

    Fix curved surface

--- src/ttmask/curved_surface.py
+++ src/ttmask/curved_surface.py
@@ -7,18 +7,32 @@ from ._cli import cli
 from .soft_edge import add_soft_edge
 from .box_setup import box_setup
 
-def curved_surface(sidelength: int, fit_sphere_diameter: float, soft_edge_width: int, pixel_size: float) -> np.ndarray:
-    fit_sphere_radius = fit_sphere_diameter / 2
+def curved_surface(
+    sidelength: int, 
+    fit_sphere_diameter: float, 
+    soft_edge_width: int, 
+    pixel_size: float,
+    surface_thickness: float 
+) -> np.ndarray:
+    sphere_radius = fit_sphere_diameter / 2
 
     # establish our coordinate system and empty mask
     coordinates_centered, mask = box_setup(sidelength)
+    coordinates_shifted = coordinates_centered - ([0, sphere_radius, 0])
 
-    # determine distances of each pixel to the center
-    distance_to_center = np.linalg.norm(coordinates_centered, axis=-1)
 
-    # set up criteria for which pixels are inside the curved surface and modify values to 1.
-    inside_curved_surface = distance_to_center < (fit_sphere_radius / pixel_size)
-    mask[inside_curved_surface] = 1
+    #determine distances of each pixel to the center
+    distance_to_center = np.linalg.norm(coordinates_shifted, axis=-1)
+
+
+    # set up criteria for which pixels are inside the sphere and modify values to 1.
+    inside_sphere = distance_to_center < (sphere_radius / pixel_size)
+    mask[inside_sphere] = 1
+
+    # if requested, criteria set up for pixels within the hollowed area and these values changed to zero
+    if surface_thickness != 0:
+        within_hollowing = distance_to_center < ((sphere_radius - surface_thickness) / pixel_size)
+        mask[within_hollowing] = 0
 
     # if requested, a soft edge is added to the mask
     mask = add_soft_edge(mask, soft_edge_width)
@@ -32,8 +46,9 @@ def curved_surface_cli(
     soft_edge_width: int = typer.Option(0),
     pixel_size: float = typer.Option(1),
     output: Path = typer.Option(Path("curved_surface.mrc")),
+    surface_thickness: float = typer.Option(...),
 ):
-    mask = curved_surface(sidelength, fit_sphere_diameter, soft_edge_width, pixel_size)
+    mask = curved_surface(sidelength, fit_sphere_diameter, soft_edge_width, pixel_size, surface_thickness)
 
     # Save the mask to an MRC file
     with mrcfile.new(output, overwrite=True) as mrc:
